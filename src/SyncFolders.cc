b4cff78c1a9d4ca39eed7a5bfd14915fc353d775
Ane794 <Ane794@outlook.com>
2021-11-05 18:21:10 +0800

    refactor: 修改項目結構

--- src/SyncFolders.cc
+++ src/SyncFolders.cc
@@ -1,9 +1,32 @@
-#include <sync-folders/SyncFolders.h>
+#include <SyncFolders.h>
 
 #include <fstream>
 #include <regex>
 #include <iostream>
 #include <yaml-cpp/yaml.h>
+
+int main() {
+    /** 配置文件路径 */
+    const std::string CONF_PATH("config.yml");
+    /** robocopy 参数 */
+    const std::string ROBOCOPY_ARGS("/mir /unicode");
+
+    SyncFolders syncFolders(CONF_PATH, ROBOCOPY_ARGS);
+    if (syncFolders.readConf() && syncFolders.generateCommands()) {
+        char choice = 0;
+        while (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n') {
+            std::cout << "Execute? (Y/N):";
+            std::cin >> choice;
+        }
+        if (choice == 'Y' or choice == 'y') {
+            auto code = syncFolders.execute();
+            std::cout << "Finished with exit code " << code << std::endl
+                      << std::endl;
+        }
+    }
+
+    return 0;
+}
 
 SyncFolders::SyncFolders(std::string confPath, std::string robocopyArgs) :
         CONF_PATH(std::move(confPath)),
